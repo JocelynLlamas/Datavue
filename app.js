6aa0c9f86c0d7e30d8631ece59a41d4a9a40049e
Jocelyn Llamas <mjlldlt@hotmail.com>
2024-01-08 13:59:26 -0600

    Getting list of categories

--- app.js
+++ app.js
@@ -202,6 +202,17 @@ new Vue({
    },
 
    computed: {
+      categories() {
+         let categories = this.products.map(el => el.category);
+         // Set object to array
+         return Array.from(new Set(categories))
+            .sort((a, b) => {
+               if (a < b) return -1;
+               else if (a > b) return 1;
+               else return 0;
+            })
+
+      },
       productsPaginated() {
          let start = (this.currentPage - 1) * this.perPage;
          let end = this.currentPage * this.perPage;
@@ -262,7 +273,7 @@ new Vue({
             }
 
             $(this.$refs.vuemodal).modal('hide');
-         }else{
+         } else {
             alert("Please fill in the form properly")
          }
       },
